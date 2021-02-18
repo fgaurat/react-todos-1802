33c4f24903132043325326310d9d12283c1f5710
Frederic GAURAT <fgaurat@eolem.com>
2021-02-18 15:05:44 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
+      <h2>TodoForm</h2>
+      <TodoForm/>
+      <hr/>
       <h2>TodoList</h2>
-      <TodoList></TodoList>
+      <TodoList/>
     </div>
   );
 }
